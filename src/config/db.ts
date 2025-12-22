065399f44933633c7d4057b019baf5648099849a
Shanjid Ahmed Rafi <shanjidahmed66@gmail.com>
2025-12-23 00:44:54 +0600

    add vehicels and booking schema

--- src/config/db.ts
+++ src/config/db.ts
@@ -17,6 +17,31 @@ const initDB = async () => {
         updated_at TIMESTAMP DEFAULT NOW()
         )
         `);
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS vehicles(
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(100) NOT NULL,
+        type VARCHAR(30) NOT NULL,
+        registration_number VARCHAR(150) UNIQUE NOT NULL,
+        daily_rent_price NUMERIC(10,2) NOT NULL CHECK (daily_rent_price > 0),
+        availability_status VARCHAR(100) NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS bookings(
+        id SERIAL PRIMARY KEY,
+        customer_id INT REFERENCES users(id) ON DELETE CASCADE,
+        vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
+        rent_start_date DATE NOT NULL,
+        rent_end_date DATE NOT NULL,
+        total_price NUMERIC(10,2) NOT NULL CHECK (total_price > 0),
+        created_at TIMESTAMP DEFAULT now(),
+        updated_at TIMESTAMP DEFAULT now(),
+        CONSTRAINT rent_dates_check CHECK (rent_end_date > rent_start_date)
+        )
+    `);
 };
 
 export default initDB;
